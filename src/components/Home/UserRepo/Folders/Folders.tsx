ae8ede50ae7010a15cd3f894dbde0d027cfbec65
RodionMas <rrssrssrrr1997@bk.ru>
2024-08-22 00:55:10 +0300

    create folder fix

--- src/components/Home/UserRepo/Folders/Folders.tsx
+++ src/components/Home/UserRepo/Folders/Folders.tsx
@@ -4,12 +4,12 @@ import Folder from "./Folder/Folder";
 import { useAppDispatch, useAppSelector } from "../../../../store/hooks";
 import { fetchColorFolder, fetchGetAllFiles, fetchGetFolder } from "../../../../store/foldersSlice";
 import { selectFolders } from "../../../../selectors/selectors";
-import NoFolders from "./NoFolders/NoFolders";
 import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { v4 as uuidv4 } from "uuid";
+import NoFolders from "./NoFolders/NoFolders";
 
 const Folders: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -39,26 +39,26 @@ const Folders: React.FC = () => {
     <div className={style.wrapper}>
       <h1 className={style.title}>Folders</h1>
       <div className={style.grid}>
-        <Swiper
-          slidesPerView={3}
-          spaceBetween={30}
-          navigation={{
-            nextEl: `.${style.swiperButtonNext}`,
-            prevEl: `.${style.swiperButtonPrev}`,
-          }}
-          modules={[Navigation]}
-          className="mySwiper"
-        >
-          {folders.length === 0 ? (
-            <NoFolders />
-          ) : (
-            folders.map((folder) => (
+        {folders.length === 0 ? (
+          <NoFolders />
+        ) : (
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            navigation={{
+              nextEl: `.${style.swiperButtonNext}`,
+              prevEl: `.${style.swiperButtonPrev}`,
+            }}
+            modules={[Navigation]}
+            className="mySwiper"
+          >
+            {folders.map((folder) => (
               <SwiperSlide key={uuidv4()}>
                 <Folder {...folder} />
               </SwiperSlide>
-            ))
-          )}
-        </Swiper>
+            ))}
+          </Swiper>
+        )}
         <div className={style.swiperButtonPrev}></div>
         <div className={style.swiperButtonNext}></div>
       </div>
